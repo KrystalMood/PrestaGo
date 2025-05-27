ebb3a7adcdaa841e7718b7da4b3cac3713468537
Knoyan Mitsu <yanappuowo@yandex.com>
2025-05-27 22:21:19 +0700

    Update lecturer sidebar: enhance UI and add 'Laporan Prestasi' section

--- resources/views/components/sidebar/mobile/lecturer.blade.php
+++ resources/views/components/sidebar/mobile/lecturer.blade.php
@@ -11,7 +11,7 @@
         </button>
     </div>
 
-    <div class="p-4">
+    <div class="p-4 overflow-y-auto max-h-[calc(100vh-4rem)]">
         <ul class="space-y-1">
             <div class="py-2 mt-2">
                 <span class="px-3 text-xs font-semibold text-gray-400 uppercase">Pembimbingan</span>
@@ -22,7 +22,7 @@
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
                     </svg>
-                    <span class="font-medium">Mahasiswa</span>
+                    <span class="font-medium">Mahasiswa Bimbingan</span>
                 </a>
             </li>
 
@@ -40,7 +40,16 @@
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
                     </svg>
-                    <span class="font-medium">Lomba</span>
+                    <span class="font-medium">Informasi Lomba</span>
+                </a>
+            </li>
+            
+            <li>
+                <a class="flex items-center p-3 rounded-lg {{ request()->routeIs('lecturer.reports.*') ? 'bg-brand-light bg-opacity-10 text-brand' : 'hover:bg-gray-100 text-gray-700' }}">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+                    </svg>
+                    <span class="font-medium">Laporan Prestasi</span>
                 </a>
             </li>
         </ul>
