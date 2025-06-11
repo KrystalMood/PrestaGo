3c58f7051cb162810ff6eb4dd00ec5ee9e055e96
5atriy0 <satriyobagus47@gmail.com>
2025-06-11 21:53:22 +0700

    fix : footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -4,26 +4,6 @@
             <div class="text-sm text-gray-500 mb-4 md:mb-0">
                 © {{ date('Y') }} Jurusan Teknologi Informasi - Politeknik Negeri Malang
             </div>
-            <div class="flex space-x-4">
-                <a href="#" class="text-gray-500 hover:text-brand transition-colors">
-                    <span class="sr-only">Bantuan</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    </svg>
-                </a>
-                <a href="#" class="text-gray-500 hover:text-brand transition-colors">
-                    <span class="sr-only">Kontak</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-                    </svg>
-                </a>
-                <a href="#" class="text-gray-500 hover:text-brand transition-colors">
-                    <span class="sr-only">Website JTI</span>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9" />
-                    </svg>
-                </a>
-            </div>
         </div>
     </div>
 </footer>
